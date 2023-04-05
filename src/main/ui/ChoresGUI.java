414e3074d1765f04c12979f47c069950fb7b5b41
arun <akumar57@student.ubc.ca>
2023-04-05 10:52:04 -0700

    Phase 3

--- src/main/ui/ChoresGUI.java
+++ src/main/ui/ChoresGUI.java
@@ -34,39 +34,45 @@ public class ChoresGUI extends JFrame implements ActionListener {
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
 
-    @SuppressWarnings({"checkstyle:MethodLength", "checkstyle:SuppressWarnings"})
+
     public ChoresGUI() {
+        setButton();
+        frameSet();
+        this.jsonWriter = new JsonWriter("./data/home.json");
+        this.jsonReader = new JsonReader("./data/home.json");
+    }
+
+
+    // MODIFIES: this
+    // EFFECTS: sets the buttons
+    public void setButton() {
         addMembers.setBounds(100,160,200,40);
         addMembers.setFocusable(false);
         addMembers.addActionListener(this);
-
         addChores.setBounds(100,220,200,40);
         addChores.setFocusable(false);
         addChores.addActionListener(this);
-
         assignChores.setBounds(100,280,200,40);
         assignChores.setFocusable(false);
         assignChores.addActionListener(this);
-
         listofmembers.setBounds(100,340,200,40);
         listofmembers.setFocusable(false);
         listofmembers.addActionListener(this);
-
         listofchores.setBounds(100,400,200,40);
         listofchores.setFocusable(false);
         listofchores.addActionListener(this);
-
         saveData.setBounds(100,460,200,40);
         saveData.setFocusable(false);
         saveData.addActionListener(this);
-
         loadData.setBounds(100,520,200,40);
         loadData.setFocusable(false);
         loadData.addActionListener(this);
+    }
 
+    // MODIFIES: this
+    // EFFECTS: sets the frame structure
+    public void frameSet() {
         frame.setContentPane(label);
-
-
         frame.add(addChores);
         frame.add(addMembers);
         frame.add(assignChores);
@@ -74,23 +80,14 @@ public class ChoresGUI extends JFrame implements ActionListener {
         frame.add(listofmembers);
         frame.add(saveData);
         frame.add(loadData);
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,700);
         frame.setVisible(true);
-
-
-
-
         frame.repaint();
-
-        this.jsonWriter = new JsonWriter("./data/home.json");
-        this.jsonReader = new JsonReader("./data/home.json");
-
-
     }
 
-    @SuppressWarnings({"checkstyle:MethodLength", "checkstyle:SuppressWarnings"})
+    // MODIFIES: this
+    // EFFECTS: processes user command
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == saveData) {
@@ -100,67 +97,28 @@ public class ChoresGUI extends JFrame implements ActionListener {
             loadHome();
         }
         if (e.getSource() == addMembers) {
-            String quantity = JOptionPane.showInputDialog(this, "Enter No of Members in House: ");
-            int intQuantity = Integer.parseInt(quantity);
-
-            if (intQuantity >= 2) {
-                for (int i = 1; i <= intQuantity; i++) {
-                    String name = JOptionPane.showInputDialog(this, "Enter name of house Member No" + i);
-                    Member member = new Member(name,null);
-                    this.home.addMember(member);
-                }
-            } else {
-                JOptionPane.showMessageDialog(this, "The Number of Member should be at least 2...");
-            }
+            addMembers();
         }
 
         if (e.getSource() == addChores) {
-            int choresAdded = listOfChores.getListOfChores().size();
-            int choresToAdd = home.getListOfMembers().getListOfMembers().size()
-                    - home.getListOfChores().getListOfChores().size();
-
-
-            for (int i = 0; i < choresToAdd; i++) {
-                String name =
-                        JOptionPane.showInputDialog(this, "Enter name of House Chore No:" + (choresAdded + i + 1));
-
-                Chore chore = new Chore(name, null);
-                this.home.addChore(chore);
-            }
+            addChores();
         }
 
         if (e.getSource() == listofchores) {
-            List<Chore> chores = home.getListOfChores().getListOfChores();
-            System.out.println("list of Chores: ");
-            List<String> myList = new ArrayList<>();
-            for (Chore chore: chores) {
-                myList.add(chore.toString());
-            }
-            String message = String.join(System.lineSeparator(), myList);
-            JOptionPane.showMessageDialog(this,message);
+            listOfChores();
         }
 
         if (e.getSource() == listofmembers) {
-            List<Member> members = home.getListOfMembers().getListOfMembers();
-            System.out.println("list of Members: ");
-            List<String> myList = new ArrayList<>();
-            for (Member member: members) {
-                myList.add(member.getName());
-            }
-            String message = String.join(System.lineSeparator(), myList);
-            JOptionPane.showMessageDialog(this, message);
+            listOfMembers();
         }
 
         if (e.getSource() == assignChores) {
-            home.choresAssignment();
-            List<String> myList = new ArrayList<>();
-            for (Member member: home.getListOfMembers().getListOfMembers()) {
-                myList.add(member.getName() + " has been assigned the Chore: " + member.getAssignedChore());
-            }
-            String message = String.join(System.lineSeparator(), myList);
-            JOptionPane.showMessageDialog(this, message);
+            assignChores();
         }
     }
+
+    // MODIFIES: this
+    // EFFECTS: saves home to file
 
     private void saveHome() {
         try {
@@ -186,6 +144,82 @@ public class ChoresGUI extends JFrame implements ActionListener {
         }
 
     }
+
+    // MODIFIES: this
+    // EFFECTS: Adds Members to listOfMembers if quantity of Members >= 2 and calls addChores()
+    private void addMembers() {
+        String quantity = JOptionPane.showInputDialog(this, "Enter No of Members in House: ");
+        int intQuantity = Integer.parseInt(quantity);
+
+        if (intQuantity >= 2) {
+            for (int i = 1; i <= intQuantity; i++) {
+                String name = JOptionPane.showInputDialog(this, "Enter name of house Member No" + i);
+                Member member = new Member(name,null);
+                this.home.addMember(member);
+            }
+        } else {
+            JOptionPane.showMessageDialog(this, "The Number of Member should be at least 2...");
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add the chores to the listOfChores
+    private void addChores() {
+        int choresAdded = listOfChores.getListOfChores().size();
+        int choresToAdd = home.getListOfMembers().getListOfMembers().size()
+                - home.getListOfChores().getListOfChores().size();
+
+
+        for (int i = 0; i < choresToAdd; i++) {
+            String name =
+                    JOptionPane.showInputDialog(this, "Enter name of House Chore No:" + (choresAdded + i + 1));
+
+            Chore chore = new Chore(name, null);
+            this.home.addChore(chore);
+        }
+    }
+
+    // REQUIRES: listOfMembers is not empty
+    // MODIFIES: this
+    // EFFECTS: display listOfMembers
+    private void listOfMembers() {
+        List<Member> members = home.getListOfMembers().getListOfMembers();
+        System.out.println("list of Members: ");
+        List<String> myList = new ArrayList<>();
+        for (Member member: members) {
+            myList.add(member.getName());
+        }
+        String message = String.join(System.lineSeparator(), myList);
+        JOptionPane.showMessageDialog(this, message);
+    }
+
+    // REQUIRES: listOfChores is not empty
+    // MODIFIES: this
+    // EFFECTS: display listOfChores
+    private void listOfChores() {
+        List<Chore> chores = home.getListOfChores().getListOfChores();
+        System.out.println("list of Chores: ");
+        List<String> myList = new ArrayList<>();
+        for (Chore chore: chores) {
+            myList.add(chore.toString());
+        }
+        String message = String.join(System.lineSeparator(), myList);
+        JOptionPane.showMessageDialog(this,message);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: assign chores randomly to members
+    private void assignChores() {
+        home.choresAssignment();
+        List<String> myList = new ArrayList<>();
+        for (Member member: home.getListOfMembers().getListOfMembers()) {
+            myList.add(member.getName() + " has been assigned the Chore: " + member.getAssignedChore());
+        }
+        String message = String.join(System.lineSeparator(), myList);
+        JOptionPane.showMessageDialog(this, message);
+    }
+
+
 
 
 }
